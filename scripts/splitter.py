426ca968a918736f8aa1ca8c35c8054e6855e9e8
Evgenii <jenyhlevnoy@gmail.com>
2024-07-19 15:35:52 +0300

    Add test split and resulting json of chunks

--- scripts/splitter.py
+++ scripts/splitter.py
@@ -14,8 +14,25 @@ splits_list = []
 
 html_splitter = HTMLHeaderTextSplitter(headers_to_split_on)
 
-with open('../data/table_row_links.txt', 'r', encoding='utf-8') as file:
-    initial_links = {line.strip() for line in file}
+# Fot test
+initial_links = {
+    "https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.channels/-channel/is"
+    "-closed-for-send.html",
+    "https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.channels/-closed"
+    "-receive-channel-exception/index.html",
+    "https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.channels/-closed-send"
+    "-channel-exception/index.html",
+    "https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.channels/-conflated"
+    "-broadcast-channel/index.html",
+    "https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.channels/-conflated"
+    "-broadcast-channel/open-subscription.html",
+    "https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.channels/-conflated"
+    "-broadcast-channel/value.html"
+    }
+
+# Uncomment when will need to parse everything
+# with open('../data/table_row_links.txt', 'r', encoding='utf-8') as file:
+#     initial_links = {line.strip() for line in file}
 
 for link in initial_links:
     print(f"Processing: {link}")
@@ -30,7 +47,7 @@ for link in initial_links:
         footer = main_content.find('footer')
         copy_icon = main_content.find('span', class_='copy-icon')
         breadcrumbs = main_content.find('div', class_='breadcrumbs')
-        link_popup = main_content.find('span', class_='copy-popup-icon')
+        link_popup = main_content.find('div', class_='copy-popup-wrapper')
         schema_block = main_content.find('div', class_='sample-container')
         feedback = main_content.find('div', class_='feedback')
         if schema_block:
@@ -50,15 +67,15 @@ for link in initial_links:
 
         html_header_splits = html_splitter.split_text(main_html)
 
-        chunk_size = 300
-        chunk_overlap = 40
+        chunk_size = 500
+        chunk_overlap = 100
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=chunk_size, chunk_overlap=chunk_overlap
         )
 
         splits = text_splitter.split_documents(html_header_splits)
         for split in splits:
-            splits_list.append({"split": split})
+            splits_list.append({"chunk": split.page_content, "metadata": split.metadata})
 
 with open('../data/splits.json', 'w', encoding='utf-8') as file:
     json.dump(splits_list, file)
