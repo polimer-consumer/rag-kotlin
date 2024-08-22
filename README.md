8a937492fa1a46303743f06bece22b1445591d61
Evgenii <jenyhlevnoy@gmail.com>
2024-08-22 10:55:36 +0300

    Add average metrics to readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Generator metrics
+
+- **answer_relevancy**: 0.9074
+- **faithfulness**: 0.7398
+- **context_recall**: 0.9219
+- **context_precision**: 0.7204
--- scripts/metrics.py
+++ scripts/metrics.py
@@ -19,6 +19,15 @@ with open('../data/test_set_answers.json', 'r') as file:
 data = Dataset.from_dict(data_json)
 score = evaluate(data, metrics=[answer_relevancy, faithfulness, context_recall, context_precision])
 df = score.to_pandas()
+
+pure_score = df[["answer_relevancy", "faithfulness", "context_recall", "context_precision"]].mean()
+markdown_content = "# Generator metrics\n\n"
+for column, avg in pure_score.items():
+    markdown_content += f"- **{column}**: {avg:.4f}\n"
+
+with open("../README.md", "w") as file:
+    file.write(markdown_content)
+
 md = df.to_markdown(index=False)
 
 file_path = '../data/metrics.md'
